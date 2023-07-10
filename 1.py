ed4cc31461645d7a3030b5abe52a45c7825d8d05
Ananya1402 <79527717+Ananya1402@users.noreply.github.com>
2023-07-10 11:46:21 +0530

    Update 1.py
    Made a few changes

--- 1.py
+++ 1.py
@@ -2,10 +2,12 @@
 # Python Program to calculate the square root
 
 # Note: change this value for a different result
-num = 8 
+num = 25 
 
 # To take the input from the user
 #num = float(input('Enter a number: '))
 
 num_sqrt = num ** 0.5
+num_sqr = num * num
 print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+print('The square is ',num_sqr)
